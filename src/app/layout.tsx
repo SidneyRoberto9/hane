7628e273982fa0da39597c09305dbf21da92ddc9
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-10-18 18:53:54 -0300

    💄 style: Create NavBar with login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from 'react';
 import { Inter } from 'next/font/google';
 
 import { cn } from '@/lib/utils';
+import { Navbar } from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className="light">
       <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+        <Navbar />
         {children}
       </body>
     </html>
